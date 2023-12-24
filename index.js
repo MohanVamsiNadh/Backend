4ac343a98c7cdb2f90c5acfb4921e506e30a1e1d
baramsivaramireddy <baramsivarami@gmail.com>
2023-12-24 13:20:35 +0530

    express added

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 require('dotenv').config()
 const dbconnect = require('./dbconnect')
-dbconnect()
+
+
+const express = require('express');
+const app = express()
+
+app.get('/' , (req,res) =>{
+    res.status(200).json({message:"hello"})
+})
+app.listen(300,() =>{
+    console.log('server started ')
+    dbconnect()
+})
