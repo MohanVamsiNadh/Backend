66fde1f567f80c38b5c2c0ce217289d205d2054d
baramsivaramireddy <baramsivarami@gmail.com>
2023-12-24 14:25:29 +0530

    basic request setup

--- index.js
+++ index.js
@@ -7,20 +7,22 @@
 require("dotenv").config();
 const dbconnect = require("./dbconnect");
 const express = require("express");
+const path = require('path');
 const app = express();
 const swaggerUi = require("swagger-ui-express");
 const YAML = require("yamljs");
-const swaggerDocument = YAML.load("./api-spec.yaml");
-global.__configurations = require("./config.js");
+const swaggerDocument = YAML.load(path.resolve(__dirname,'api-spec.yaml'));
+global.__configurations = require(path.resolve(__dirname,'config.js'));
 global.ROUTE_DIR = __dirname + "/routes/api";
 global.CONTROLLER_DIR = __dirname + "/controllers";
 global.UTIL_DIR = __dirname + "/utils";
-
 global.DB_MODEL = __dirname + "/models";
+app.use(express.json())
 app.use((req, res, next) => {
   if (req.originalUrl == "/") {
     res.status(200).send();
   } else {
+    console.debug(`Incoming request -> method: ${req.method},  query params: ${JSON.stringify(req.query)},param: ${JSON.stringify(req.params)},url: ${req.originalUrl}, body: ${JSON.stringify(req.body)}`);
     next();
   }
 });
