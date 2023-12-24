6f7ac3fc3dd5f23789f9875109d515c1bdd7c293
baramsivaramireddy <baramsivarami@gmail.com>
2023-12-24 19:38:37 +0530

    css property changed

--- index.js
+++ index.js
@@ -35,7 +35,11 @@ app.use((req, res, next) => {
 */
 const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui.min.css";
 
-app.use(`/api-docs`, swaggerUi.serve, swaggerUi.setup(swaggerDocument ,{ customCssUrl: CSS_URL }));
+app.use(`/api-docs`, swaggerUi.serve, swaggerUi.setup(swaggerDocument ,{
+  customCss:
+    '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+  customCssUrl: CSS_URL,
+}));
 app.get("/health-check", async (req, res) => {
   await dbconnect();
   res.status(200).json({ message: "server is up" });
