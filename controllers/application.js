00ec54ac6de99fde330041f8bc233cb14af02c99
baramsivaramireddy <baramsivarami@gmail.com>
2024-02-25 03:16:21 +0530

    email service added

--- controllers/application.js
+++ controllers/application.js
@@ -1,18 +1,22 @@
 
 const path = require('path');
+const SendMail = require('../utils/sendmail');
 const UserApplication = require(path.resolve(DB_MODEL,'userApplication')); 
 const dbconnect = require(path.resolve(__dirname,'..','dbconnect'))
 const ApplicationStage = require(path.resolve(DB_MODEL,'applicationStage')); 
+const User = require(path.resolve(DB_MODEL, 'user'));
 module.exports = {
   create: async (req, res) => {
     await dbconnect();
     try {
 
       let firstStage = await ApplicationStage.findOne({applicationType:req.body.application_typeId ,order:1})
-     
+  
       req.body.currentStage = firstStage._id
       let newResource = await UserApplication.create(req.body);
-     
+      let userId = req.query.userId
+      let user= await User.findById(userId)
+      await   SendMail('thank you for applying ' , user.email, "Application Received")
       res.status(201).json({ message: newResource._id });
     } catch (err) {
       console.log(`Error while creating user application`);
@@ -42,9 +46,13 @@ module.exports = {
     await dbconnect();
     try {
       let resourceId = req.params.id;
-
+    
+     
+     
 
       let applicationDoc  =await UserApplication.findById(resourceId).populate('currentStage')
+
+      let user= await User.findById(applicationDoc.userId)
       if ( req.body.status == 'rejected')
       {
         let resource = await UserApplication.findByIdAndUpdate(
@@ -52,13 +60,14 @@ module.exports = {
           {status: 'rejected'}
         );
 
+        await   SendMail('application rejected sorry to inform ' , user.email, "Application rejected")
         return res.status(201).json({message: "updated"})
 
       }
       else if (req.body.status == 'approved') {
 
         let allStages =  await ApplicationStage.find({applicationType:applicationDoc.application_typeId})
-
+       
         
 
         let nextStage = allStages.filter((s) => s.order == applicationDoc.currentStage.order+1)
@@ -73,7 +82,7 @@ module.exports = {
 
         applicationDoc.status = 'approved'
         applicationDoc.save()
-
+        await   SendMail(' application approved ' , user.email, "Application approved")
         return res.status(200).json({})
       }
       
