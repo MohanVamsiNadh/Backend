71d48a1b204689d5d3220e602739c6eca41cdff2
baramsivaramireddy <baramsivarami@gmail.com>
2024-02-25 03:36:36 +0530

    officer2 filter

--- controllers/application.js
+++ controllers/application.js
@@ -132,7 +132,7 @@ module.exports = {
 
       let allApplications = await UserApplication.find({}).populate('currentStage').populate('userId').populate('application_typeId')
 
-      let filteredApplications= allApplications.filter((application) => application.currentStage.roleId == req.query.roleId && application.status !=  'rejected' && application.status != 'approved')
+      let filteredApplications= allApplications.filter((application) => application.currentStage.roleId == req.query.roleId && application.status == 'pending')
       return res.status(200).json({data : filteredApplications})
     }
 
